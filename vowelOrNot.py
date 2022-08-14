334039814d954c00a5b3442ae96859e77bfe4687
shubham singh <92996831+singhshubham593@users.noreply.github.com>
2022-08-14 11:44:54 +0530

    python : to given word is vowel or not.

---
+++ vowelOrNot.py
@@ -0,0 +1,5 @@
+word =input("Enter the word : ")
+if word=="A"or word=="E" or word=="I" or word=="O" or word=="U" or word=="a" or word=="e"or word=="i"or word=="o" or word=="u":
+    print("It is vowel.")
+else:
+    print("It is not vowel.")
