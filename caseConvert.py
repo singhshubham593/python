2b9720b35d6b7dac28b27b8f010dddfce95455da
shubham singh <92996831+singhshubham593@users.noreply.github.com>
2022-08-16 21:45:01 +0530

    Python :  convert uppercase into lowercase  and vice verse

---
+++ caseConvert.py
@@ -0,0 +1,7 @@
+word =input("Enter the word in upper case: ")
+#to convert upper in lower case:
+if (word.isupper()):
+    print(word.lower())
+#To convert lower in upper case:
+if (word.islower()):
+   print(word.upper())
