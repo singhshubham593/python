78b9cb9321910f6227c6ebf608a33f27a903eb80
shubham singh <92996831+singhshubham593@users.noreply.github.com>
2022-08-19 20:47:14 +0530

    Python : to show day name by input number from 1 to 7.

---
+++ dayName.py
@@ -0,0 +1,26 @@
+#take number input
+num =int(input("Enter the number to find day : "))
+#condition for find day
+if (num==1):
+    print("Day is Monday")
+    print("")
+elif (num==2):
+    print("Day is tuesday")
+
+elif (num==3):
+    print("Day is wednesday")
+
+elif (num==4):
+    print("Day is thursday")
+
+elif (num==5):
+    print("Day is friday")
+
+elif (num==6):
+    print("Day is saturday")
+
+elif (num==7):
+    print("Day is sunday")
+
+else :
+    print("Day is not exist")
